cffd112f77cedf9451e0021b792f26be9d943e25
Suhani Sharma <146577777+suhani-sharmaa@users.noreply.github.com>
2025-07-03 23:10:44 +0530

    Delete Week3_SpringCore_Maven directory

--- Week3_SpringCore_Maven/code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-//A simple repo class that simulates saving a book
-public class BookRepository {
-    //Method to simulate saving a book to the database
-    public void saveBook(String bookName) {
-        System.out.println("Book saved: " + bookName);
-    }
-}
